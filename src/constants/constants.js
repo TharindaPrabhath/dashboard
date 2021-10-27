b9ed3b46618d83aa0393e63e32335840d53b8658
tharinda <tharindahp@gmail.com>
2021-10-27 23:48:11 +0530

    defined opp types

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,6 +1,8 @@
 /* eslint-disable prettier/prettier */
 export const USER_TYPES = ['School Leaver', 'Undergraduate', 'Graduate'];
 
+export const OPP_TYPES = ['Workshops', 'Undergraduate', 'Graduate'];
+
 export const SUBSCRIPTION_TYPES = ['Yearly', 'Monthly'];
 
 export const GENDERS = ['Male', 'Female'];
